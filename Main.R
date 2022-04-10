c4fe1343477d93f207426fe362043364bf85e8b0
Teeuwen <a.s.teeuwen@utwente.nl>
2022-04-10 18:16:21 +0200

    Initialised main sxript sourcing all other scripts

---
+++ Main.R
@@ -0,0 +1,8 @@
+# Main script sourcing all other scripts
+
+# Author: Aleid Sunniva Teeuwen
+# Date: 04.10.2022
+# Project: FoodSecGovSim2
+# Publication: A systematic review of simulation studies that assess the impact of food security governance measures
+
+
